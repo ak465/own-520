eb8f5c4247514394ff98481c9662618be06fc4fc
ak465 <76836592+ak465@users.noreply.github.com>
2021-05-07 20:58:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+var  Alex_left, Alex_right, alex_jump, alex_down, alex_car;
+var monster, monster_left, monster_right, monster_sleep;
+var carsGroup;
+var Canvas, backgroundImage;
+var score;
+var gameState=0;
+var rainGroup;
+var hitsound, checkpointsound, backgroundsound, diesound;
+var carImage, alexImage, monsterImage, houseImage;
+var ground;
+var car;
+
+var engine, world;
+
+function preload(){
+ backgroundImage=loadImage("backgroundImage.gif");
+ monsterImage=loadImage("monsterImage.png");
+ alexImage=loadImage("alexImage.png");
+ alex_jump=loadImage("alex jump.png");
+ alex_down=loadImage("alex_down.png");
+ alex_car=loadImage("images.jpeg");
+
+}
+
+function setup(){
+  createCanvas(1200, 700);
+  engine=Engine.create();
+  world=engine.world;
+  
+  alex= new Alex(3, 350, 300, 300);
+  monster= new Monster(900, 350, 300, 300);
+
+  
+  
+  //carsGroup= createGroup();
+  //rainGroup= createGroup();
+
+  score=0;
+
+
+  
+}
+
+function draw(){
+    background(backgroundImage);
+    Engine.update(engine);
+
+    //Text("score:"+ score,500,50);
+
+       alex.display();
+       monster.display();
+       alex.keys();
+       
+}
